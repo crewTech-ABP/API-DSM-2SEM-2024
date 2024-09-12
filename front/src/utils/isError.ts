611656ae7c666b7fbc68938cd8597800372efc08
lucasroqe <alvim.lucas2@hotmail.com>
2024-09-12 16:54:24 -0300

    teste

--- front/src/utils/isError.ts
+++
@@ -1,4 +0,0 @@
-import { ErrorProps } from "../types";
-
-// Os objetos recebidos do servidor que são mensagens de erro possuem a estrutura {error:""}
-export const isErrorProps = (object: any): object is ErrorProps => "error" in object;
--- taco/src/controllers/index.ts
+++
@@ -1,9 +0,0 @@
-import FieldController from "./FieldController";
-import CategoryController from "./CategoryController";
-import FoodController from "./FoodController";
-
-export {
-    CategoryController,
-    FieldController,
-    FoodController
-};
